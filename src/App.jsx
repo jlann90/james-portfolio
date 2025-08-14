83947b2e7c24bec74687c0e2ce812374d34fcdf6
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-08-14 16:57:25 -0700

    feat: create and style Menu component, add content to App to test what will be future Homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import Menu from "./components/Menu.jsx";
+import ProfilePhoto from "./assets/ProfilePhoto.png";
 
 function App() {
   return (
     <>
-      <Menu />
+      <main className="flex justify-between bg-th-db pl-[84px] pr-[24px] h-screen overflow-hidden">
+        <div className="pt-[120px] max-w-[700px]">
+          <img src={ProfilePhoto} width="248"></img>
+          <h1 className="climate-crisis text-[64px] uppercase pt-[30px] pl-[10px] text-th-yw drop-shadow-title">
+            James
+            <br /> Lannon
+          </h1>
+          <p className="barolow-bold text-th-wh text-2xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            ornare ultrices varius. Donec enim libero, cursus vel dui vitae,
+            tempor lacinia nunc. Nam elementum ipsum suscipit massa posuere
+            tempus. Phasellus consectetur justo lorem, vitae pretium orci
+            aliquam nec. Cras gravida turpis vel velit sollicitudin tempor vitae
+            eu mauris. Sed nunc urna, iaculis ac pellentesque nec, vulputate sed
+            neque. In porta ex lorem, in rhoncus lacus mollis sed. Ut commodo
+            erat sed dapibus aliquam.
+          </p>
+        </div>
+        <Menu />
+      </main>
     </>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,23 @@
 export default function Menu() {
+  const menuListClass = "text-right pt-[24px] pr-[24px]";
+  const menuBtnClass =
+    "climate-crisis text-2xl uppercase text-th-db hover:text-th-lb";
+
   return (
-    <section>
-      <nav>
+    <section className="pt-[36px] pl-[102px] h-screen">
+      <nav className="w-[282px] h-[101vh] bg-th-yw drop-shadow-menu rounded-xl outline-6 outline-th-db">
         <ul>
-          <li className="climate-crisis text-th-db">
-            <button>Home</button>
+          <li className={menuListClass}>
+            <button className={menuBtnClass}>Home</button>
           </li>
-          <li className="climate-crisis">
-            <button>Projects</button>
+          <li className={menuListClass}>
+            <button className={menuBtnClass}>Projects</button>
           </li>
-          <li class className="climate-crisis">
-            <button>Experience</button>
+          <li className={menuListClass}>
+            <button className={menuBtnClass}>Experience</button>
           </li>
-          <li className="climate-crisis">
-            <button>Contact</button>
+          <li className={menuListClass}>
+            <button className={menuBtnClass}>Contact</button>
           </li>
         </ul>
       </nav>
