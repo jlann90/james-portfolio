61372a9826e716594a2c11c7ff9b4d03c75f7dda
James Lannon <jameslannon@Jamess-MacBook-Pro.local>
2025-08-14 23:03:47 -0700

    feat: add conditional classes for active menu links and remove the hover class option for active menu links

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -3,15 +3,17 @@ import { useMenu } from "../context/MenuContext.jsx";
 export default function MenuItems({ menuId, children }) {
   const { menuSelection, handleMenuSelect } = useMenu();
   const menuListClass = "text-right pt-[24px] pr-[24px]";
-  const menuBtnClass =
-    "climate-crisis text-2xl uppercase text-th-db hover:text-th-lb";
+  const menuBtnClass = "climate-crisis text-2xl uppercase text-th-db";
+
   const isActive = menuSelection.menuId === menuId;
+  const hoverClass = isActive ? "" : "hover:text-th-lb";
 
   return (
     <li className={menuListClass}>
       <button
+        disabled={isActive}
         onClick={() => handleMenuSelect(menuId)}
-        className={`${menuBtnClass}${isActive ? " active" : ""}`}
+        className={`${menuBtnClass} ${hoverClass}${isActive ? " text-th-rd" : ""}`}
       >
         {children}
       </button>
